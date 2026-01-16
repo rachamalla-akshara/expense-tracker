8acad0f15d9d6911508da361e89344c944e858b1
Rachamalla Akshara <rrachamallaakshara@gmail.com>
2026-01-16 21:51:56 +0530

    Dockerize Flask backend, tested container on localhost:5001

--- backend/run.py
+++ backend/run.py
@@ -5,4 +5,4 @@ app = create_app()
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5001)
+    app.run(host="0.0.0.0", port=5001, debug=True)
